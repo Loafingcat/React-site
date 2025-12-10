9554a78d8c5a02653fa537ede6465c2bb154ac56
Junho Byun <bjunho0130@gmail.com>
2025-12-10 10:56:40 +0900

    cors 삭제

--- server.js
+++ server.js
@@ -17,18 +17,6 @@ const pool = new Pool({
 
 
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] // 허용할 HTTP 메서드 명시
-}));
-
 // 3. PostgreSQL 연결 테스트
 pool.connect((err, client, release) => {
     if (err) {
